10c9b4d283883f15cc3b9d9a40378f3f07f1831a
besthor <igbebestor7@gmail.com>
2023-02-11 23:40:42 +0000

    still testing

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
-
-"""A variable storage, an instance of FileStorage"""
-storage = FileStorage()
-storage.reload()
